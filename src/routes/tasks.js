08be63880268ee5e92f9ad48dad8175290cf81d2
urmee04 <urmee04sust@gmail.com>
2025-09-07 21:58:30 -0400

    add task API routes with CRUD operations

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,38 +1,50 @@
-const express = require('express');
-
-const Task = require('../models/task');
+import express from "express";
+import Task from "../models/task.js";
 
 const router = express.Router();
 
-
-router.get('/', (req, res) => {
-  Task.find({})
-    .then(tasks => res.json(tasks))
-    .catch(err => res.status(500).json({ error: err }));
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find({}).sort({ createdAt: -1 });
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.post('/add', (req, res) => {
-  const { title } = req.body;
-  const newTask = new Task({ title });
-
-  newTask.save()
-    .then(task => res.json(task))
-    .catch(err => res.json(500, err));
+router.post("/add", async (req, res) => {
+  try {
+    const { title } = req.body;
+    const newTask = new Task({ title });
+    const savedTask = await newTask.save();
+    res.json(savedTask);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.delete('/delete/:id', (req, res) => {
-  const id = req.params.id;
-
-  Task.findByIdAndDelete(id)
-    .then(task => res.json(task))
-    .catch(err => res.json(500, err));
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedTask = await Task.findByIdAndDelete(id);
+    res.json(deletedTask);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-router.post('/update/:id', (req, res) => {
-  const { done } = req.body;
-  Task.findByIdAndUpdate(req.params.id, { done })
-    .then(task => res.json(task))
-    .catch(err => res.json(500, err));
+router.post("/update/:id", async (req, res) => {
+  try {
+    const { done } = req.body;
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      { done },
+      { new: true } //return the updated document
+    );
+    res.json(updatedTask);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-module.exports = router;
+export default router;
